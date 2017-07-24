83fa9631eecacae102e674539be7e4d724f7736b
Orontion <orontion@gmail.com>
2017-07-25 01:57:25 +0300

    Refactored COM-port settings logic and interface

--- src/interfc/settingswindow/view/SettingsWindowController.java
+++ src/interfc/settingswindow/view/SettingsWindowController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import jssc.SerialPort;
 
 public class SettingsWindowController {
 	@FXML
@@ -24,21 +25,55 @@ public class SettingsWindowController {
 	private ComboBox<Integer> cmbPortSpeed;
 	
 	@FXML
+	private ComboBox<Integer> cmbPortDataBits;
+	
+	@FXML
+	private ComboBox<String> cmbPortStopBits;
+	
+	@FXML
+	private ComboBox<String> cmbPortParity;
+	
+	@FXML
 	private Button butConfirmSerialPortSettings;
 	
 	@FXML
 	private void initialize(){
 		serialPortObsList = FXCollections.observableArrayList();
-		portSpeedObsList = FXCollections.observableArrayList();
-		
-		portSpeedObsList.add(7200);
-		portSpeedObsList.add(9600);
-		portSpeedObsList.add(14400);
-		portSpeedObsList.add(57600);
-		portSpeedObsList.add(115200);
-		
 		cmbSerialPort.setItems(serialPortObsList);
+		
+		portSpeedObsList = FXCollections.observableArrayList();
+		portSpeedObsList.add(SerialPort.BAUDRATE_4800);
+		portSpeedObsList.add(SerialPort.BAUDRATE_9600);
+		portSpeedObsList.add(SerialPort.BAUDRATE_14400);
+		portSpeedObsList.add(SerialPort.BAUDRATE_57600);
+		portSpeedObsList.add(SerialPort.BAUDRATE_115200);
 		cmbPortSpeed.setItems(portSpeedObsList);
+		
+		portDataBits = FXCollections.observableArrayList();
+		portDataBits.add(SerialPort.DATABITS_5);
+		portDataBits.add(SerialPort.DATABITS_6);
+		portDataBits.add(SerialPort.DATABITS_7);
+		portDataBits.add(SerialPort.DATABITS_8);
+		cmbPortDataBits.setItems(portDataBits);
+		cmbPortDataBits.setValue(SerialPort.DATABITS_8);
+		
+		//При выборе использовать индекс + 1
+		portStopBits = FXCollections.observableArrayList();
+		portStopBits.add("1");
+		portStopBits.add("2");
+		portStopBits.add("1,5");
+		cmbPortStopBits.setItems(portStopBits);
+		cmbPortStopBits.setValue("1");
+		
+		//При выборе использовать индекс
+		portParity = FXCollections.observableArrayList();
+		portParity.add("NONE");
+		portParity.add("ODD");
+		portParity.add("EVEN");
+		portParity.add("MARK");
+		portParity.add("SPACE");
+		cmbPortParity.setItems(portParity);
+		cmbPortParity.setValue("NONE");
 		
 		//TODO Добавить загрузку текущих настроек COM-порта из ядра программы
 	}
@@ -80,6 +115,12 @@ public class SettingsWindowController {
 	private ObservableList<String> serialPortObsList;
 	
 	private ObservableList<Integer> portSpeedObsList;
+	
+	private ObservableList<Integer> portDataBits;
+	
+	private ObservableList<String> portStopBits;
+	
+	private ObservableList<String> portParity;
 	
 	//Метод установки ссылки на объект окна настроек
 	public void setSettingsWindowRef(SettingsWindowClass stgWinRef){
